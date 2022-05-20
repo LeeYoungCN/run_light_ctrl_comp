1806f3e95fa5e5cd77d33c626db1a0b5b276d7da
Li Yang <42291832+LeeYoungCN@users.noreply.github.com>
2022-05-20 09:40:43 +0800

    Update LedDriver.h
    123

--- src/stub/LedDriver.h
+++ src/stub/LedDriver.h
@@ -6,6 +6,5 @@
 #define LED_OFF 0
 
 VOS_VOID DRV_LedCtl(VOS_UINT16 ledId, VOS_UINT8 u8Status);
-VOS_INT32 DRV_PwmDutyWrite(VOS_UINT16 u16GptTimeId, VOS_UINT8 u8ChannelNo, VOS_UINT32 u32Compare);
 
 #endif // LEDDRIVER_H
